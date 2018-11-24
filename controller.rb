c68f22fd6ee647b94f0996f87570f7fd7157ac19
Philip Winnington <ftcpjw@yahoo.com>
2018-11-23 20:48:06 -0500

    Added come class definntions

--- controller.rb
+++ controller.rb
@@ -2,6 +2,8 @@ class LogParserController
   def initialize 
     @log_file - LogFile.new
     @current_view = FleDialogView.new
+    @current_view.clear_display
+    @current_view.set_cursor
   end
 
   def run
--- view.rb
+++ view.rb
@@ -1,9 +1,24 @@
 class BasicView
+  def clear_display
+    print "\e[2J"
+  end
+  def set_cursor row = 1, column = 1
+    print "\e[#{row};#{column}H"
+  end
+  def center text
+    colums = $stdin.winsize[1]
+    text_length = text.length
+    colum_location = colums / 2 - text_length / 2
+    "\e[#{colum_location}G#{text}"
+  end
+  def red text
+    "\e[31;40m#{text}\e[0m"
+  end
 end
 
 class FileDialogView < BasicView
   def display 
-    puts "Select an Apache log file."
+    puts red(center("Select an Apache log file."))
   end
 end
 
